fdbba318690276c0f59a777de445be2aa1547486
bluespace9214@gmail.com <reddyraja@Reddyrajas-MacBook-Pro.local>
2013-05-17 19:06:18 +0530

    Spring Data Hadoop project for Pivotal HD cluster. Separate project checked in.

---
+++ spring-hadoop-wordcount-gphd/src/main/java/com/pivotal/springhadoop/wordcount/WordCount.java
@@ -0,0 +1,57 @@
+package com.pivotal.springhadoop.wordcount;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class WordCount {
+
+	private static final Log log = LogFactory.getLog(WordCount.class);
+
+	public static void main(String[] args) throws Exception {
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext(
+				"/applicationContext.xml", WordCount.class);
+		log.info("Streaming Application Running");
+		context.registerShutdownHook();
+	}
+
+	public static class TokenizerMapper extends
+			Mapper<Object, Text, Text, IntWritable> {
+
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
+
+	public static class IntSumReducer extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+}
---
+++ spring-hadoop-wordcount-gphd/src/main/scripts/add-to-maven.sh
@@ -0,0 +1,13 @@
+export PIVOTAL_INSTAL_DIR=/usr/lib/gphd
+
+mvn install:install-file -Dfile=libs/spring-data-hadoop-1.0.1.RC1.jar -DgroupId=org.springframework.data -DartifactId=spring-data-hadoop -Dversion=1.0.1.RC1 -Dpackaging=jar
+mvn install:install-file -Dfile=$PIVOTAL_INSTAL_DIR/hadoop-2.0.2_alpha_gphd_2_0_1_0/hadoop-auth-2.0.2-alpha-gphd-2.0.1.0.jar -DgroupId=gphd  -DartifactId=gphd-auth -Dversion=2.0.1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=$PIVOTAL_INSTAL_DIR/hadoop-2.0.2_alpha_gphd_2_0_1_0/hadoop-common-2.0.2-alpha-gphd-2.0.1.0.jar -DgroupId=gphd     -DartifactId=gphd-common -Dversion=2.0.1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=$PIVOTAL_INSTAL_DIR/hadoop-hdfs-2.0.2_alpha_gphd_2_0_1_0/hadoop-hdfs-2.0.2-alpha-gphd-2.0.1.0.jar -DgroupId=gphd     -DartifactId=gphd-hdfs -Dversion=2.0.1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=$PIVOTAL_INSTAL_DIR/hadoop-mapreduce-2.0.2_alpha_gphd_2_0_1_0/hadoop-mapreduce-client-common-2.0.2-alpha-gphd-2.0.1.0.jar -DgroupId=gphd     -DartifactId=gphd-client-common -Dversion=2.0.1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=$PIVOTAL_INSTAL_DIR/hadoop-mapreduce-2.0.2_alpha_gphd_2_0_1_0/hadoop-mapreduce-client-core-2.0.2-alpha-gphd-2.0.1.0.jar -DgroupId=gphd     -DartifactId=gphd-client-core -Dversion=2.0.1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=$PIVOTAL_INSTAL_DIR/hadoop-mapreduce-2.0.2_alpha_gphd_2_0_1_0/hadoop-mapreduce-client-jobclient-2.0.2-alpha-gphd-2.0.1.0.jar -DgroupId=gphd   -DartifactId=gphd-client-jobclient -Dversion=2.0.1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=$PIVOTAL_INSTAL_DIR/gphd/hadoop-mapreduce-2.0.2_alpha_gphd_2_0_1_0/hadoop-mapreduce-examples-2.0.2-alpha-gphd-2.0.1.0.jar -DgroupId=gphd     -DartifactId=gphd-examples -Dversion=2.0.1.0 -Dpackaging=jar
+nvn install:install-file -Dfile=$PIVOTAL_INSTAL_DIR/hadoop-yarn-2.0.2_alpha_gphd_2_0_1_0/hadoop-yarn-api-2.0.2-alpha-gphd-2.0.1.0.jar -DgroupId=gphd     -DartifactId=gphd-yarn-api -Dversion=2.0.1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=$PIVOTAL_INSTAL_DIR/hadoop-yarn-2.0.2_alpha_gphd_2_0_1_0/hadoop-yarn-common-2.0.2-alpha-gphd-2.0.1.0.jar -DgroupId=gphd     -DartifactId=gphd-yarn-client -Dversion=2.0.1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=$PIVOTAL_INSTAL_DIR/hadoop-yarn-2.0.2_alpha_gphd_2_0_1_0/hadoop-yarn-common-2.0.2-alpha-gphd-2.0.1.0.jar -DgroupId=gphd     -DartifactId=gphd-yarn-common -Dversion=2.0.1.0 -Dpackaging=jar
