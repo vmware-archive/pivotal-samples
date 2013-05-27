356d1d9180bbcd584e879a7ceff7b44870d3e7d8
Rajdeep Dua <dua_rajdeep@yahoo.com>
2013-05-27 16:45:41 +0530

    Project for SpringData JDBC show casing Count Businesses in a city

---
+++ spring-data-jdbc/spring-jdbc-count-business-in-city/src/main/java/com/pivotal/springdata/jdbc/business/BusinessCount.java
@@ -0,0 +1,54 @@
+package com.pivotal.springdata.jdbc.business;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+public class BusinessCount {
+    ApplicationContext context;
+    DataSource dataSource;
+    JdbcTemplate template;
+    
+	public BusinessCount() {
+		context = new ClassPathXmlApplicationContext("datasources-beans.xml");
+		dataSource = context.getBean("postGresDataSource", DataSource.class);
+		template = new JdbcTemplate(dataSource);
+	}
+	
+	public void executeQuery() {
+		List<RowRecord> rowRecords = template.query(
+				"select city,count(*) a from business group by city",
+				new RowMapper<RowRecord>() {
+					public RowRecord mapRow(ResultSet rs, int rowNum) throws SQLException {
+						RowRecord row = new RowRecord();
+						row.city = rs.getString("city");
+						row.count = rs.getInt("a");
+						return row;
+					}
+				});
+		System.out.println("Returned: "+ rowRecords);
+	}
+	
+	class RowRecord {
+		String city;
+		int count;
+		public String toString() {
+			String s = "{" +
+				   "city=" + city + "," +
+				   "count=" + count + 
+					"}";
+			return s;
+		}
+	}
+    public static void main( String[] args ){
+    	BusinessCount app = new BusinessCount();
+    	app.executeQuery();	
+    }
+}
