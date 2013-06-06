016a8b02f4917dc8108e41f9b54bce1cb4823df8
Rajdeep Dua <dua_rajdeep@yahoo.com>
2013-06-06 17:01:28 +0530

    Added new files for gpxf

---
+++ hawq/gpxf_tables/drop_gpxf_tables.sql
@@ -0,0 +1,10 @@
+drop external table retail_demo.categories_dim_gpxf;
+drop external table retail_demo.customer_addresses_dim_gpxf;
+drop external table retail_demo.customers_dim_gpxf;
+drop external table retail_demo.date_dim_gpxf;
+drop external table retail_demo.email_addresses_dim_gpxf;
+drop external table retail_demo.order_lineitems_gpxf;
+drop external table retail_demo.orders_gpxf;
+drop external table retail_demo.payment_methods_gpxf;
+drop external table retail_demo.products_dim_gpxf;
+
