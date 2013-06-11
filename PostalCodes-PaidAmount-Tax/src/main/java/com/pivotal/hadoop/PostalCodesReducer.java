216614ebe9c88622f3b39cee5a0378351ff7d04c
qvantel <qvantel@QSSLP085.(none)>
2013-06-11 15:14:37 +0530

    Java map reduce Top ten postalcodes of highest paidamount and taxes

---
+++ PostalCodes-PaidAmount-Tax/src/main/java/com/pivotal/hadoop/PostalCodesReducer.java
@@ -0,0 +1,65 @@
+package com.pivotal.hadoop;
+
+import java.io.IOException;
+import java.util.TreeMap;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class PostalCodesReducer extends
+		Reducer<IntWritable, Text, NullWritable, Text> {
+
+	static TreeMap<Double, Text> recordRepo = new TreeMap<Double, Text>();
+
+	@Override
+	protected void reduce(IntWritable key, Iterable<Text> counts,
+			Context context) throws IOException, InterruptedException {
+		StringBuffer temp;
+		double total_tax_amount = 0;
+		double total_paid_amount = 0;
+		Text result = new Text();
+		for (Text val : counts) {
+
+			String[] rawTokens = val.toString().split(",");
+			StringBuffer tax_amount = new StringBuffer(rawTokens[0]);
+			StringBuffer paid_amount = new StringBuffer(rawTokens[1]);
+
+			total_paid_amount = total_paid_amount
+					+ Double.parseDouble(paid_amount.substring(paid_amount
+							.indexOf("p:") + 2));
+
+			total_tax_amount = total_tax_amount
+					+ Double.parseDouble(tax_amount.substring(tax_amount
+							.indexOf("t:") + 2));
+
+		}
+
+		temp = new StringBuffer(key.toString());
+		temp.append("\t");
+		temp.append(new StringBuffer(String.valueOf(total_paid_amount)));
+		temp.append("\t");
+		temp.append(new StringBuffer(String.valueOf(total_tax_amount)));
+
+		result.set(temp.toString());
+
+		recordRepo.put(total_paid_amount, result);
+		if (recordRepo.size() > 10) {
+			recordRepo.remove(recordRepo.firstKey());
+		}
+
+	}
+
+	@Override
+	protected void cleanup(org.apache.hadoop.mapreduce.Reducer.Context context)
+			throws IOException, InterruptedException {
+
+		for (Text t : recordRepo.descendingMap().values()) {
+			context.write(NullWritable.get(), t);
+
+		}
+		super.cleanup(context);
+
+	}
+}
---
+++ PostalCodes-PaidAmount-Tax/src/test/java/com/pivotal/hadoop/PostalCodesPaidAmountTaxTest.java
@@ -0,0 +1,67 @@
+package com.pivotal.hadoop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * See the <a
+ * href="https://cwiki.apache.org/confluence/display/MRUNIT/Index">MRUnit
+ * Wiki</a> for more information.
+ */
+public class PostalCodesPaidAmountTaxTest {
+	MapReduceDriver<LongWritable, Text, IntWritable, Text, NullWritable, Text> mapReduceDriver;
+	MapDriver<LongWritable, Text, IntWritable, Text> mapDriver;
+	ReduceDriver<IntWritable, Text, NullWritable, Text> reduceDriver;
+/*
+	@Before
+	public void setUp() {
+		PostalCodesMapper mapper = new PostalCodesMapper();
+		PostalCodesReducer reducer = new PostalCodesReducer();
+		mapDriver = new MapDriver<LongWritable, Text, IntWritable, Text>();
+		mapDriver.setMapper(mapper);
+		reduceDriver = new ReduceDriver<IntWritable, Text, NullWritable, Text>();
+		reduceDriver.setReducer(reducer);
+		mapReduceDriver = new MapReduceDriver<LongWritable, Text, IntWritable, Text, NullWritable, Text>();
+		mapReduceDriver.setMapper(mapper);
+		mapReduceDriver.setReducer(reducer);
+	}
+
+	@Test
+	public void testMapper() {
+		mapDriver.withInput(new LongWritable(1), new Text("7149696583	16877852	52	2010-10-04 21:41:54	2010-10-08 05:51:03			CreditCard	149.45425	2173.88000	1	0.00000	BOGO-PDX	0.50000	N	N	N			26 KITTYHAWK			PENGILLY	MN	55770	USA	(676)090-4274	Mark Spivey	Mark.Spivey@tivocommunity.com	OS53372-931651-04-77852	http://myretailsite.emc.com/product_detail/Lawn_&_Patio/?"));
+		mapDriver.withOutput(new IntWritable(55770), new Text("p:149.45425,t:2173.88000"));
+		mapDriver.runTest();
+	}
+
+	@Test
+	public void testReducer() {
+		List<Text> values = new ArrayList<Text>();
+		values.add(new Text("p:149.45425,t:2173.88000"));
+		values.add(new Text("p:149.45425,t:2173.88000"));
+		values.add(new Text("p:149.45425,t:2173.88000"));
+
+		reduceDriver.withInput(new IntWritable(55770), values);
+		reduceDriver.withOutput(null, new Text("55770	6521.64	448.36275"));
+		reduceDriver.runTest();
+	}
+
+	@Test
+	public void testMapReduce() {
+		mapReduceDriver.withInput(new LongWritable(1), new Text("cat cat dog"));
+		mapReduceDriver.addOutput(new Text("cat"), new LongWritable(2));
+		mapReduceDriver.addOutput(new Text("dog"), new LongWritable(1));
+		mapReduceDriver.runTest();
+	}
+
+*/
+}
