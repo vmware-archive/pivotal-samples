d1a307ff261aca035c67033711e8611bb805bfe4
dyozie <dyozie@gopivotal.com>
2013-11-06 11:17:46 -0800

    script changes from Michael Goddard

--- hawq/hawq_tables/load_data_to_HDFS.sh
+++ hawq/hawq_tables/load_data_to_HDFS.sh
@@ -4,17 +4,9 @@ base_dir="/retail_demo"
 
 # Clean up any previous load
 echo "hadoop fs -rm -r -skipTrash $base_dir"
-hadoop fs -rm -r -skipTrash $base_dir 
+hadoop fs -rm -r -skipTrash $base_dir
 
-echo "hadoop fs -mkdir $base_dir"
-hadoop fs -mkdir $base_dir
-
-for file in *.tsv.gz
-do
-  dir=`echo $file | perl -ne 's/^(.+?)\..+$/$1/;print;'`
-  echo "hadoop fs -mkdir $base_dir/$dir"
-  hadoop fs -mkdir $base_dir/$dir
-  echo "hadoop fs -put $file $base_dir/$dir/"
-  hadoop fs -put $file $base_dir/$dir/
-done
+# Copy the data directory, recursively, into HDFS root
+echo "hadoop fs -put /retail_demo /"
+hadoop fs -put /retail_demo /
 
